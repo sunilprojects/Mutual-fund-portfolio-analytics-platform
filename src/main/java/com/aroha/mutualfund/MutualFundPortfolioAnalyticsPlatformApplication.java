43409acc5beb915e3074939a26983cde83c2b385
rajith_suvarna <rajithsuvarna49@gmail.com>
2025-06-29 22:05:57 +0530

    Initial project setup code push to integration branch

---
+++ src/main/java/com/aroha/mutualfund/MutualFundPortfolioAnalyticsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aroha.mutualfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutualFundPortfolioAnalyticsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutualFundPortfolioAnalyticsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aroha/mutualfund/MutualFundPortfolioAnalyticsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aroha.mutualfund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutualFundPortfolioAnalyticsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
