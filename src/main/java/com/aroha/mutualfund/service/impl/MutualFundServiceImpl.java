44b81354153e94395d470394993c7ccc491f04a1
sunilprojects <sunildrives77@gmail.com>
2025-06-30 17:59:46 +0530

    Controller upload method declared

--- src/main/java/com/aroha/mutualfund/service/impl/MutualFundServiceImpl.java
+++ src/main/java/com/aroha/mutualfund/service/impl/MutualFundServiceImpl.java
@@ -1,10 +1,16 @@
 package com.aroha.mutualfund.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.aroha.mutualfund.service.MutualFundService;
 
 @Service
 public class MutualFundServiceImpl implements MutualFundService {
+	   @Override
+	    public String processFundFile(MultipartFile[] file) {
+	       
+	      return null;
+	    }
 
 }
